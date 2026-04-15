b4c50434dcbad8710e83656d30ce2954da2758fa
IronLeaf <146615281+nem-web@users.noreply.github.com>
2026-04-16 03:09:18 +0530

    Update cash threshold for alert from 700k to 7M

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ Total: $${total}`
             if (diffTime > 0) profitPerHour = Math.floor(diffCash / diffTime);
         }
 
-        if (cash > 700000) {
+        if (cash > 7000000) {
             await sendTelegram(`💰 Cash Alert: $${cash.toLocaleString()}`);
         }
 
